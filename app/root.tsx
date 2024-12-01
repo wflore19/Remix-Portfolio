606a7aca0f66408bfca08f123c7bd6c2394ef852
wflore19 <wflore19@montgomerycollege.edu>
2024-11-30 23:55:51 -0500

    chore: change theme

--- app/root.tsx
+++ app/root.tsx
@@ -37,7 +37,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Links />
 			</head>
 			<body>
-				<Theme accentColor="gold" appearance="dark" radius="large">
+				<Theme accentColor="gold" appearance="dark" radius="small">
 					{children}
 				</Theme>
 				<ScrollRestoration />
