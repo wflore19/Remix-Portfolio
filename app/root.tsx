0bbf544795d60442531cc1ec72a123e0f1083a91
wflore19 <wflore19@montgomerycollege.edu>
2024-12-05 02:16:23 -0500

    chore: change theme

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import type { LinksFunction } from "@remix-run/node";
 
 import "./styles.css";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 export const links: LinksFunction = () => [
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -37,8 +37,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Links />
 			</head>
 			<body>
-				<Theme accentColor="gold" appearance="dark" radius="small">
+				<Theme accentColor="blue" appearance="dark" radius="small">
 					{children}
+					<ThemePanel />
 				</Theme>
 				<ScrollRestoration />
 				<Scripts />
