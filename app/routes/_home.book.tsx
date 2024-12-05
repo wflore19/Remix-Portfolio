ed73ba146e5b5ae4cfc0d12885a3db24bf716447
wflore19 <wflore19@montgomerycollege.edu>
2024-12-05 02:20:05 -0500

    fix: auth logic

--- app/routes/_home.book.tsx
+++ app/routes/_home.book.tsx
@@ -8,80 +8,68 @@ import {
 	Text,
 	TextArea,
 } from "@radix-ui/themes";
-import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { ActionFunctionArgs } from "@remix-run/node";
 import {
 	Await,
 	Form,
 	redirect,
-	useFetcher,
 	useLoaderData,
+	useRouteLoaderData,
 } from "@remix-run/react";
 import { RiEditLine } from "@remixicon/react";
 import React, { Suspense } from "react";
 import { GoogleButton } from "~/components/googleButton";
 import {
-	GuestBookEntry,
 	createGuestBookEntry,
 	getGuestBookEntries,
+	GuestBookEntry,
 } from "~/drizzle/queries/book";
-import { getGoogleAuthURL } from "~/utils/auth";
 import { getTimeAgo } from "~/utils/format";
-import { ensureUserAuthenticated, user } from "~/utils/session.server";
+import {
+	ensureUserAuthenticated,
+	getSession,
+	user,
+} from "~/utils/session.server";
+import { HomeData } from "./_home";
 
-export async function loader({ request }: LoaderFunctionArgs) {
-	const session = await ensureUserAuthenticated(request);
+export async function loader() {
 	const guestBook = await getGuestBookEntries();
 
-	let userId;
-	if (session) {
-		userId = user(session);
-		return { guestBook, userId, googleAuthURL: undefined };
-	}
-
-	const googleAuthURL: string = getGoogleAuthURL();
-
-	return { guestBook, userId: undefined, googleAuthURL };
+	return guestBook;
 }
 
 export default function BookPage() {
-	const { guestBook, userId, googleAuthURL } = useLoaderData<
-		typeof loader
-	>() as {
-		guestBook: GuestBookEntry[];
-		userId: number | undefined;
-		googleAuthURL: string | undefined;
-	};
+	const { hasAuth, googleAuthURL } = useRouteLoaderData(
+		"routes/_home"
+	) as HomeData;
+	const guestBook = useLoaderData() as GuestBookEntry[];
 
-	const fetcher = useFetcher();
 	const [textAreaValue, setTextAreaValue] = React.useState("");
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		fetcher.submit(event.currentTarget, { method: "post" });
+	React.useEffect(() => {
 		setTextAreaValue("");
-	};
+	}, []);
+
 	return (
 		<Suspense fallback={<Spinner />}>
 			<Await resolve={[guestBook]}>
 				<Box my={"5"}>
-					{!userId ? (
+					{!hasAuth ? (
 						<Flex direction={"column"} gap={"3"} align={"center"}>
 							<Text>Sign the guest book</Text>
 							<GoogleButton href={`${googleAuthURL}`} />
 						</Flex>
 					) : (
 						<Box my={"5"}>
-							<Form method="post" onSubmit={handleSubmit}>
+							<Form method="post" reloadDocument>
 								<Flex direction="column" gap="2">
 									<TextArea
 										name="message"
 										size={"3"}
-										placeholder="What's on your mind?"
+										placeholder="Leave a message :)"
 										value={textAreaValue}
 										onChange={(event) => {
-											setTextAreaValue(
-												event.target.value
-											);
+											setTextAreaValue(event.target.value);
 										}}
 									/>
 									<Box>
@@ -104,19 +92,11 @@ export default function BookPage() {
 									<Box width={"full"}>
 										<Flex justify={"between"}>
 											<Box>
-												<Flex
-													gap="3"
-													align="center"
-													mb="2">
+												<Flex gap="3" align="center" mb="2">
 													<Avatar
-														src={
-															post.profilePicture ||
-															undefined
-														}
+														src={post.profilePicture || undefined}
 														radius="full"
-														fallback={
-															post.firstName![0]
-														}
+														fallback={post.firstName![0]}
 													/>
 													<Flex
 														direction="column"
@@ -137,9 +117,7 @@ export default function BookPage() {
 																initial: "1",
 																md: "2",
 															}}>
-															{getTimeAgo(
-																post.createdAt!
-															)}
+															{getTimeAgo(post.createdAt!)}
 														</Text>
 													</Flex>
 												</Flex>
@@ -156,11 +134,7 @@ export default function BookPage() {
 									</Box>
 
 									{idx !== post.message!.length && (
-										<Separator
-											my="5"
-											size="4"
-											color="gray"
-										/>
+										<Separator my="5" size="4" color="gray" />
 									)}
 								</Box>
 							);
@@ -173,18 +147,21 @@ export default function BookPage() {
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-	const session = await ensureUserAuthenticated(request);
-	if (!session) {
+	const hasAuth = await ensureUserAuthenticated(request);
+	if (!hasAuth) {
 		throw redirect("/book");
 	}
 
-	const userId = user(session);
-
-	const formData = await request.formData();
-	const message = String(formData.get("message"));
-
 	try {
-		await createGuestBookEntry(userId, message);
+		const formData = await request.formData();
+		const session = await getSession(request);
+		const userId = user(session);
+
+		const message = formData.get("message");
+
+		if (!message) throw new Error("Message is required");
+
+		await createGuestBookEntry(userId, message.toString());
 		return redirect("/book");
 	} catch (error) {
 		throw new Error("Failed to create post");
